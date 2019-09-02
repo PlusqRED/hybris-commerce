44e80f7dc6095389cc201c5cebb587f870f03b14
Vinograd Oleg <rickes.oleg@gmail.com>
2019-09-02 12:43:59 +0300

    Added new theme

---
+++ bin/custom/training/traininginitialdata/gensrc/org/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 02.09.2019 12:01:01                         ---
+ * ----------------------------------------------------------------
+ */
+package org.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
